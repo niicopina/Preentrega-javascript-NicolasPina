00d87e9d728af1f71b4ac616dd27cce8258441be
nicopina <pinanicolasagustin@gmail.com>
2022-12-06 14:24:30 -0300

    nueva funcion

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-let estado = prompt("elija entre celeste, amarillo o rojo segun la bandera del balneario");
-if(estado === "celeste"){
-    alert("puede nadar en el mar")
-}else if(estado === "amarillo"){
-    alert("nade con precaucion")
-}if(estado === "rojo"){
-    alert("es peligroso nadar")
-};
 
-class Comida{
-    constructor(plato, cantidad, adicional){
-    this.plato = plato;
-    this.cantidad = cantidad;
-    this.adicional = adicional;
+const productos = [
+    {id: 1, nombre: "milanesa", precio: 1000},
+    {id: 2, nombre: "pizza", precio: 1500},
+    {id: 3, nombre: "tarta", precio: 700},
+    {id: 4, nombre: "hamburguesa", precio: 1200},
+ ];
+
+const buscar = (nombre) => {
+    let producto = productos.find(item => item.nombre === nombre);
+    if (producto){
+        let mensaje = `
+        id: ${producto.id}
+        Nombre: ${producto.nombre}
+        Precio: ${producto.precio} `;
+
+        alert(mensaje);
+    }else{
+        alert("producto no encontrado");
     }
-    vender(){
-        this.cantidad -= 1;
-    }
-}
-const plato1 = new Comida(prompt("ingrese plato de comida que desee pedir"), parseInt(prompt("ingrese cantidad")), prompt("ingrese adicional"));
-let mensaje = `sumado al carro de compras`;
-alert(mensaje);
+};
+let nombre = prompt("ingrese el plato deseado")
+buscar(nombre);
+
+
+
+
