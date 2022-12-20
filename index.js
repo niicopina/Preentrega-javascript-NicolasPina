a0fce748763b162a9a5333f86159c5646250e901
nicopina <pinanicolasagustin@gmail.com>
2022-12-20 00:28:07 -0300

    suma de productos

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let contenedor = document.getElementById("contenedor1");
+//let contenedor = document.getElementById("contenedor1");
 const productos = [
     {id: 1, nombre: "clasico", precio: 5000},
     {id: 2, nombre: "despertar dulce", precio: 4500},
-    {id: 3, nombre: "mix cumpleañitos", precio: 5700},
-    {id: 4, nombre: "festivos", precio: 5200},
+    {id: 3, nombre: "mix cumpleañitos", precio: 3700},
+    {id: 4, nombre: "festivos", precio: 6200},
  ];
 
-const buscar = (nombre) => {
+let nombre = prompt("ingrese el desayuno deseado entre Clasico, Despertar dulce, Mix cumpleañitos, Festivos");
+
+let producto = productos.find(item => item.nombre === nombre);
+if (producto){
+    let mensaje = `
+            Id: ${producto.id}
+            Nombre: ${producto.nombre}
+            Precio: ${producto.precio}
+            `;
+            alert(mensaje);}else if
+                (producto == "");alert("no aplica");
+                
+            
+            
+
+
+     /* const buscar = (nombre) => {
     let producto = productos.find(item => item.nombre === nombre);
     if (producto){
         let mensaje = `
@@ -14,27 +30,30 @@ const buscar = (nombre) => {
         Nombre: ${producto.nombre}
         Precio: ${producto.precio} `;
         alert(mensaje);
-    }else if(producto === false){
+            }else if(producto === false);
         alert("producto no encontrado");
-        }else{
-            producto = prompt("producto no encontrado, intente nuevamente");
-            
-        }
-/* let nombre = prompt("ingrese el desayuno deseado entre Clasico, Despertar dulce, Mixcumpleañitos, Festivos")
-buscar(nombre); */
+            }; */
+            /* let cantidad = productos.find(item => (item.precio + cantidad) === precio);
+            if (cantidad){
+                let mensaje = `
+                Total: ${cantidad.precio}`;
+                alert(mensaje);
+            }; */
+
+//let  = prompt("ingrese el desayuno deseado entre Clasico, Despertar dulce, Mixcumpleañitos, Festivos");
+        
+
 
 let cantidad = parseInt(prompt("defina la cantidad a comprar"));
-let total = producto.precio * cantidad;
-alert(total);}
-
-
+        let total = producto.precio * cantidad;
+        alert(total);
 
 /* const totales = productos.map(item => {
     return {
         nombre: item.nombre,
         precio: item.precio * cantidad
     };
-}); */
+});
 
 /* productos.forEach(producto => {
     let div = document.createElement("div");
@@ -47,17 +66,17 @@ alert(total);}
 
 //Eventos
 
-let btn = document.getElementById("btn");
+//let btn = document.getElementById("btn");
 
 /* function ejecutar(){
     console.log("click en el boton");
 } */
-const ejecutar = () => {
-    console.log("click en el boton");
-}
-//btn.addEventListener("click", ejecutar); ------------> primera forma
-//btn.onclick = ejecutar;------------------------------> segunda forma
+//const ejecutar = () => {
+    //console.log("click en el boton");
 
-boton.addEventListener("click", () => ejecutar("hey"));
+//btn.addEventListener("click", ejecutar); ------------> primera forma
+//btn.onclick = () =>{console.log("respuesta 2")}
+
+//boton.addEventListener("click", () => ejecutar("hey"));
 
 
